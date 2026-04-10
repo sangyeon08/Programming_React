8cc01e8d7ab717ce4b1460cf3765a1e230a27fb5
slee <sanoni0115@gmail.com>
2026-04-10 09:02:02 +0900

    feat: add classname, todolist.css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,14 +1,19 @@
+import './todolist.css';
+
 function TodoListApp() {
     return (
         <div className="todo">
-            <h1 className="todo__title">No Think, Do IT</h1>
+            <h1 className="todo__title">No Think, Do It.</h1>
             <form className="todo__form">
                 <input type="text" placeholder="할 일을 입력하세요!" className="todo__input"/>
                 <button type="submit" className="todo__button todo__button--add">Add</button>
             </form>
-            <ul>
-                <li>
-                    <input type="checkbox" id="chk-2"/>
+            <ul className="todo__list">
+                <li className="todo__item todo__item--empty">
+                    <p>할 일 없음</p>
+                </li>
+                <li className="todo__item todo__item--complete">
+                    <input type="checkbox" id="chk-2" className="todo__check"/>
                     <label htmlFor="chk-2" className="todo__label">think</label>
                     <button type="button" className="todo__button todo__button__edit">✏️</button>
                     <button type="button" className="todo__button todo__button__delete">❌</button>
