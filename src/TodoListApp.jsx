9695b1941888a470ae6f944992d304f9b0bc400d
slee <sanoni0115@gmail.com>
2026-04-10 13:09:14 +0900

    [Feat] : Add Components

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,26 +1,26 @@
-import './todolist.css';
+import './todolist.css'
+import TodoItemEmpty from './components/TodoItemEmpty.jsx'
+import Button from './components/Button.jsx'
+import Checkbox from './components/Checkbox.jsx'
 
 function TodoListApp() {
     return (
         <div className="todo">
             <h1 className="todo__title">No Think, Do It.</h1>
             <form className="todo__form">
-                <input type="text" placeholder="할 일을 입력하세요!" className="todo__input"/>
-                <button type="submit" className="todo__button todo__button--add">Add</button>
+                <input type="text" placeholder="할 일을 입력하세요." className="todo__input" />
+                <Button type="submit" className="todo__button todo__button--add">Add</Button>
             </form>
             <ul className="todo__list">
-                <li className="todo__item todo__item--empty">
-                    <p>할 일 없음</p>
-                </li>
+                <TodoItemEmpty />
                 <li className="todo__item todo__item--complete">
-                    <input type="checkbox" id="chk-2" className="todo__check"/>
-                    <label htmlFor="chk-2" className="todo__label">think</label>
-                    <button type="button" className="todo__button todo__button__edit">✏️</button>
-                    <button type="button" className="todo__button todo__button__delete">❌</button>
+                    <Checkbox id="1" />
+                    <Button className="todo__button todo__button--edit">✏️</Button>
+                    <Button className="todo__button todo__button--delete">❌</Button>
                 </li>
             </ul>
         </div>
     )
 }
 
-export default TodoListApp
+export default TodoListApp;
