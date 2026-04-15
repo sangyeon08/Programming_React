641c36cb6f0f25a82c0f4583fb269e8b9324d4b6
slee <sanoni0115@gmail.com>
2026-04-15 10:09:35 +0900

    [Feat] : AddTodo

--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,6 +1,6 @@
 import Button from './Button.jsx'
 
-export default function TodoAdder() {
+export default function TodoAdder({ addTodo }) {
     return (
         <form className="todo__form">
             <input type="text" placeholder="할 일을 입력하세요." className="todo__input" />
