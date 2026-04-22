aad7f0739c2ef82bf8588856af7c9211bcf903b0
slee <sanoni0115@gmail.com>
2026-04-22 09:28:34 +0900

    [Chore]: TodoListApp show todos

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,36 +1,36 @@
 import { useState } from 'react';
 import './todolist.css'
-import TodoItemEmpty from './components/TodoItemEmpty.jsx'
-import Button from './components/Button.jsx'
-import Checkbox from './components/Checkbox.jsx'
+// import TodoItemEmpty from './components/TodoItemEmpty.jsx'
+// import Button from './components/Button.jsx'
+// import Checkbox from './components/Checkbox.jsx'
 import TodoHeader from './components/TodoHeader.jsx'
 import TodoAdder from './components/TodoAdder.jsx'
-import TodoItem from './components/TodoItem.jsx'
+// import TodoItem from './components/TodoItem.jsx'
 import TodoList from './components/TodoList.jsx'
 
 class Todo {
-    constructor(id, text, isCompleted) {
-        this.id = Date.now(); // 할 일 고유의 값 new Date().getTime()과 동일한 역할을 하는 코드
-        this.text = text; // 할 일의 내용
-        this.isCompleted = false; // 할 일의 완료 여부
+    constructor(text) {
+        this.id = Date.now();   //할일 id: 고유의 값 == new Date().getTime()
+        this.text = text;       //할일의 내용
+        this.isCompleted = false; //할일 완료 여부
     }
 }
 
 function TodoListApp() {
     const [todos, setTodos] = useState([]);
-    const addTodo = (text) => setTodos((todos)=> [ 
-        // 이전 todos 복사
-        ...todos, 
-        // newTodo 만들어서
-        // 뒤에 추가하기!
+    const addTodo = (text) => setTodos((todos) => [
+        //이전 todos 복사
+        ...todos,
+        //newTodo 만들어서
+        //뒤에 추가하자
         new Todo(text)
     ]);
 
     return (
         <div className="todo">
             <TodoHeader />
-            <TodoAdder addTodo = {addTodo}/>
-            <TodoList todos/>
+            <TodoAdder addTodo={addTodo} />
+            <TodoList todos={todos} />
         </div>
     )
 }
