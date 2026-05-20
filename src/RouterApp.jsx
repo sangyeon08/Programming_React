e8ad95dd486f8726c4f4174b2faa87325114575d
slee <sanoni0115@gmail.com>
2026-05-20 09:58:55 +0900

    [Feat] : Add RouterApp and useNavigate

---
+++ src/RouterApp.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import CounterApp from './CounterApp.jsx'
+import TodoListApp from './TodoListApp.jsx'
+
+function LinkButtonPageApp() {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <h1>App 목록</h1>
+            <ul>
+                <li>
+                    <Link to="/counterapp">🔢 CounterApp</Link>
+                </li>
+                <li>
+                    <Link to="/todolistapp">✅ TodoListApp</Link>
+                </li>
+                <li><button
+                    onClick={() => navigate("/counterapp")}
+                >🔢 CounterApp</button></li>
+                <li><button
+                    onClick={() => navigate("/todolistapp")}
+                >✅ TodoListApp</button></li>
+            </ul>
+        </>
+    )
+}
+
+export default function RouterApp() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LinkButtonPageApp />} />
+                <Route path="/counterapp" element={<CounterApp />} />
+                <Route path="/todolistapp" element={<TodoListApp />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
